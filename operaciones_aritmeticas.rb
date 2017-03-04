0c94df2315d52c5bd94b88e57fc399b333b30278
jonareyes <jonathan_reyesv@hotmail.com>
2017-03-03 20:57:32 -0600

    This exercise it's just to understand how to work aritmetic math

---
+++ operaciones_aritmeticas.rb
@@ -0,0 +1,17 @@
+#In this exercise we put asignation math like result on problems
+
+suma = 7 + 7
+resta = 21 - 7 
+p suma == resta
+#=>true
+
+#Second part
+
+var1 = 20
+var2 = 10
+
+resta = var1 - var2 + 4.5
+multiplicacion = resta * var1 + var2
+division = multiplicacion / 3
+p division == 100
+#=>true
